4c354d495044198e2e1dce770e051bcc2bfc5e4b
ViktorLumiste <viktor.lumiste@gmail.com>
2022-10-25 13:53:59 +0300

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ function weatherDataFetch(city) {
     })
     .then(function(data){
         console.log(data);
+        drawWeather(data)
     })
     .catch(function(){
 
@@ -14,4 +15,12 @@ function weatherDataFetch(city) {
 }
 function cityWeather(e){
     weatherDataFetch('Tallinn')
+}
+function drawWeather(data){
+    var celcius = Math.round(parseFloat(data.main.temp)-273.15);
+    var description = data.weather[0].description;
+
+    document.querySelector("#description").innerHTML = description;
+    document.querySelector("#temp").innerHTML = celcius + "&deg;";
+    document.querySelector("#location").innerHTML = data.name;
 }
